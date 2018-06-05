fb394156ade97049d0c0aa30613ca576d24f640d
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2018-06-05 00:05:28 -0300

    Corrigindo alguns erros

--- controllers/geral-meses.js
+++ controllers/geral-meses.js
@@ -4,8 +4,9 @@ const _  = require("lodash");
 
 module.exports = function (req, res, next) {
     let result = {};
-    let sql = `select COUNT(a.id) as total
+    let sql = `select COUNT(a.id) as total,SUM(vf.valor_realizado) as soma
     FROM viagem_solicitacao a 
+    inner join viagem_fechamento vf on vf.codigo_solicitacao = a.codigo_solicitacao
    GROUP BY MONTH(a.data_hora_saida) ASC`;
 
 
@@ -14,8 +15,12 @@ module.exports = function (req, res, next) {
         .then(result => {
            
             let r = _.map(result, 'total');
+            let soma = _.map(result, 'soma');
            
-            res.status(200).json(r);
+            res.status(200).json({
+                soma:soma,
+                total:r
+            });
 
         })
 }
